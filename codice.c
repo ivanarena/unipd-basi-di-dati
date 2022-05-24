cb31a1e0538aa054b17b6fbda9aa411c6b87b777
ivanarena <ivan.x28x@gmail.com>
2022-05-24 11:41:46 +0200

    Sistemato scheletro del codice C

--- codice.c
+++ codice.c
@@ -2,10 +2,36 @@
 #include "dependencies/include/libpq-fe.h"
 #include <stdlib.h>
 
+#define PG_HOST "127.0.0.1" // oppure " localhost " o " postgresql "
+#define PG_USER "postgres"  // il vostro nome utente
+#define PG_DB "soundexp"    // il nome del database
+#define PG_PASS "Abc280102" // la vostra password
+#define PG_PORT 5432
+
 int main()
 {
+    char conninfo[250];
 
-    printf("Hello WOolrd!!");
+    sprintf(conninfo, "user=%s password=%s dbname=%s hostaddr=%s port=%d",
+            PG_USER, PG_PASS, PG_DB, PG_HOST, PG_PORT);
+
+    PGconn *conn = PQconnectdb(conninfo);
+
+    if (PQstatus(conn) == CONNECTION_BAD)
+    {
+        fprintf(stderr, "Connessione al database fallita: %s\n",
+                PQerrorMessage(conn));
+        PQfinish(conn);
+        exit(1);
+    }
+    else
+    {
+        printf("Connessione al database riuscita\n");
+    }
+
+    // TODO: QUERIES
+
+    PQfinish(conn);
 
     return 0;
 }
